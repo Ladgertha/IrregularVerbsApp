7596e2aafdde99fb99eb43d1ea27737b846df702
Anna <myandroidblog@yandex.ru>
2022-04-01 22:54:40 +0300

    Add slash screen module

--- app/src/main/java/ru/ladgertha/irregularverbs/ui/IrregularVerbsActivity.kt
+++ app/src/main/java/ru/ladgertha/irregularverbs/ui/IrregularVerbsActivity.kt
@@ -1,13 +1,23 @@
 package ru.ladgertha.irregularverbs.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
 import ru.ladgertha.irregularverbs.R
+import ru.ladgertha.splash_api.SplashScreenStarter
 
 class IrregularVerbsActivity : AppCompatActivity() {
+
+    private val splashScreenStarter: SplashScreenStarter by inject {
+        parametersOf(this@IrregularVerbsActivity)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_irregular_verbs)
+        if (savedInstanceState == null) {
+            splashScreenStarter.start(R.id.irregularVerbsContainer)
+        }
     }
 }
